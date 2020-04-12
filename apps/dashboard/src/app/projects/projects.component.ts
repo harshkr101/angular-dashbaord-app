9dafab56091ca0aac679d1d2e49b82a38858c538
Harsh Kumar <developer.harsh101@gmail.com>
2020-04-12 16:24:53 +0530

    adds customers , projects

---
+++ apps/dashboard/src/app/projects/projects.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'workshop-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css']
+})
+export class ProjectsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

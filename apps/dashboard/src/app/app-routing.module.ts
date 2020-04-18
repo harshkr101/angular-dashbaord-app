129cf0714bafb87a50b913eb0578a918c893e0e2
Harsh Kumar <developer.harsh101@gmail.com>
2020-04-18 22:41:18 +0530

    adds login component and routes

--- apps/dashboard/src/app/app-routing.module.ts
+++ apps/dashboard/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import {LoginComponent} from '@workshop/ui-login';
+const routes: Routes = [
+    {path:'',component:LoginComponent},
+    {path:'**',redirectTo:'',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -8,6 +8,7 @@ import {HomeModule} from './home/home.module';
 import {ProjectsModule} from './projects/projects.module';
 import {CustomersModule} from './customers/customers.module';
 import { AppRoutingModule } from './app-routing.module';
+import { UiLoginModule } from '@workshop/ui-login';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -17,7 +18,8 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule,
     HomeModule,
     ProjectsModule,
-    CustomersModule
+    CustomersModule,
+    UiLoginModule
     
   ],
   providers: [],

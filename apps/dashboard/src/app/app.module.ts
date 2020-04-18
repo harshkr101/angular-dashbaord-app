d2ae80e9966a5234226c53d9875e4036d019bbd6
Harsh Kumar <developer.harsh101@gmail.com>
2020-04-18 22:20:23 +0530

    adds app routing module

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -7,13 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HomeModule} from './home/home.module';
 import {ProjectsModule} from './projects/projects.module';
 import {CustomersModule} from './customers/customers.module';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     BrowserAnimationsModule,
     MaterialModule,
+    AppRoutingModule,
     HomeModule,
     ProjectsModule,
     CustomersModule

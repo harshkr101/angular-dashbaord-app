4cb28276e89782674d3107efec8c4fffec9dfbd6
Harsh Kumar <developer.harsh101@gmail.com>
2020-04-19 14:56:39 +0530

    adds observables to projects

--- apps/dashboard/src/app/projects/projects.component.ts
+++ apps/dashboard/src/app/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ProjectsService,Project} from '@workshop/core-data';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'workshop-projects',
@@ -9,7 +10,7 @@ import {ProjectsService,Project} from '@workshop/core-data';
 export class ProjectsComponent implements OnInit {
 
 
-  projects:Project[];
+  projects$:Observable<Project[]>;
   selectedProject:Project;
 
   constructor(private projectsService:ProjectsService) { }
@@ -24,7 +25,7 @@ export class ProjectsComponent implements OnInit {
   }
 
   getProjects(){
-    this.projectsService.all().subscribe((result:any)=>this.projects=result);
+      this.projects$=this.projectsService.all();
   }
 
   cancel(){
